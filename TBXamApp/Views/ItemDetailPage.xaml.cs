3f548bac8288531904f6639c6bd3d919fb435f43
Andrew Lesondak <andrewlesondak@Drewdaks-Mac.attlocal.net>
2021-11-18 21:38:10 -0800

    proj setup

---
+++ TBXamApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using TBXamApp.ViewModels;
+
+namespace TBXamApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ TBXamApp/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using TBXamApp.Models;
+using TBXamApp.ViewModels;
+
+namespace TBXamApp.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
