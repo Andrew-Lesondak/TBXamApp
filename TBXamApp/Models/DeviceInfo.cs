12df596ef24418f1a4f67d3152d7ce264013d411
Andrew Lesondak <andrewlesondak@Drewdaks-Mac.attlocal.net>
2021-11-18 23:43:17 -0800

    added font awesome icons

---
+++ TBXamApp/Models/DeviceInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TBXamApp.Models
+{
+    public class DeviceInfo
+    {
+        public string InternetConnectivity { get; set; }
+        public string DeviceDetails { get; set; }
+        public string DisplayInfo { get; set; }
+    }
+}
